99f62b3aac4c26d9efdf59c0d8fde6ba4181a401
Dauren Rakhmetolla <daurenr98@gmail.com>
2025-03-16 16:30:22 +0500

    add active state?

--- src/components/TodoFilter.tsx
+++ src/components/TodoFilter.tsx
@@ -1,28 +1,28 @@
 import { useAppDispatch, useAppSelector } from '../store/hooks'
 import { setFilter } from '../store/todoSlice'
-import '../styles/TodoFilter.module.scss'
+import styles from '../styles/TodoFilter.module.scss'
 
 const TodoFilter: React.FC = () => {
   const dispatch = useAppDispatch()
   const filter = useAppSelector((state) => state.todos.filter)
 
   return (
-    <div>
+    <div className={styles['filter-buttons']}>
       <button
         onClick={() => dispatch(setFilter('all'))}
-        className={filter === 'all' ? 'active' : ''}
+        className={filter === 'all' ? 'styles.active' : ''}
       >
         Все
       </button>
       <button
         onClick={() => dispatch(setFilter('active'))}
-        className={filter === 'active' ? 'active' : ''}
+        className={filter === 'active' ? 'styles.active' : ''}
       >
         Активные
       </button>
       <button
         onClick={() => dispatch(setFilter('completed'))}
-        className={filter === 'completed' ? 'active' : ''}
+        className={filter === 'completed' ? 'styles.active' : ''}
       >
         Завершённые
       </button>
